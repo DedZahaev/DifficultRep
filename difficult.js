a8ee025c2321e7db345a3eb42ec8135cd8230a72
Danila Marychev <marychtv5@gmail.com>
2021-10-05 22:12:04 +0300

    3 задание усложненное

--- difficult.js
+++ difficult.js
@@ -1,43 +1,43 @@
 'use strict';
 
-// let lang = prompt("На каком языке укажите ru или en");
-// if (lang == "ru") {
-//     lang = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-//     for (let i = 0; i < lang.length; i++) {
-//         console.log(lang[i]);
+let lang = prompt("На каком языке укажите ru или en");
+if (lang == "ru") {
+    lang = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+    for (let i = 0; i < lang.length; i++) {
+        console.log(lang[i]);
 
-//     }
-// } else if (lang == "en") {
-//     lang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-//     for (let i = 0; i < lang.length; i++) {
-//         console.log(lang[i]);
+    }
+} else if (lang == "en") {
+    lang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    for (let i = 0; i < lang.length; i++) {
+        console.log(lang[i]);
 
-//     }
-// }
+    }
+}
 
-// switch (lang) {
-//     case "ru":
-//         lang = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-//         for (let i = 0; i < lang.length; i++) {
-//             console.log(lang[i]);
-//         }
-//         break
-//     case "en":
-//         lang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-//         for (let i = 0; i < lang.length; i++) {
-//             console.log(lang[i]);
-//         }
-//         break
+switch (lang) {
+    case "ru":
+        lang = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+        for (let i = 0; i < lang.length; i++) {
+            console.log(lang[i]);
+        }
+        break
+    case "en":
+        lang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        for (let i = 0; i < lang.length; i++) {
+            console.log(lang[i]);
+        }
+        break
 
-// }
-// let arr1 = {
-//     en: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-// };
-// let arr2 = {
-//     ru: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+}
+let arr1 = {
+    en: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+};
+let arr2 = {
+    ru: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
 
-// };
-// lang == "ru" ? console.log(arr2) : console.log(arr1);
+};
+lang == "ru" ? console.log(arr2) : console.log(arr1);
 
 
 let namePerson = prompt("Ввести Артем или Александр");
