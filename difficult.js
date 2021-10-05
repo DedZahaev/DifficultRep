da892e0615ab0f15a0667798818e691a87039047
Danila Marychev <marychtv5@gmail.com>
2021-10-05 22:27:21 +0300

    3 усложненное задание

--- difficult.js
+++ difficult.js
@@ -30,14 +30,15 @@ switch (lang) {
         break
 
 }
-let arr1 = {
-    en: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-};
-let arr2 = {
-    ru: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+let arr1 = [
 
-};
-lang == "ru" ? console.log(arr2) : console.log(arr1);
+    ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+];
+
+
+lang == "ru" ? console.log(arr1[0][0], arr1[0][1], arr1[0][2], arr1[0][3], arr1[0][4], arr1[0][5], arr1[0][6]) :
+    console.log(arr1[1][0], arr1[1][1], arr1[1][2], arr1[1][3], arr1[1][4], arr1[1][5], arr1[1][6]);
 
 
 let namePerson = prompt("Ввести Артем или Александр");
