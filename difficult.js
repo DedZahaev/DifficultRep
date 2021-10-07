95f0fdac85be9164e80144fc0b856dafa468215d
Danila Marychev <marychtv5@gmail.com>
2021-10-07 19:15:48 +0300

    Функция с тремя заданиями

--- difficult.js
+++ difficult.js
@@ -1,48 +1,14 @@
 'use strict';
 
-let lang = prompt("На каком языке укажите ru или en");
-if (lang == "ru") {
-    lang = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-    for (let i = 0; i < lang.length; i++) {
-        console.log(lang[i]);
-
+const changetext = function (per) {
+    if (typeof per != "string") {
+        alert("в качестве аргумента передана не строка");
     }
-} else if (lang == "en") {
-    lang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    for (let i = 0; i < lang.length; i++) {
-        console.log(lang[i]);
-
+    per = per.trim();
+    if (per.length > 30) {
+        per = per.slice(0, 30) + '...';
+        console.log(per);
     }
-}
 
-switch (lang) {
-    case "ru":
-        lang = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-        for (let i = 0; i < lang.length; i++) {
-            console.log(lang[i]);
-        }
-        break
-    case "en":
-        lang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-        for (let i = 0; i < lang.length; i++) {
-            console.log(lang[i]);
-        }
-        break
 
 }
-let arr1 = [
-
-    ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-];
-
-
-lang == "ru" ? console.log(arr1[0][0], arr1[0][1], arr1[0][2], arr1[0][3], arr1[0][4], arr1[0][5], arr1[0][6]) :
-    console.log(arr1[1][0], arr1[1][1], arr1[1][2], arr1[1][3], arr1[1][4], arr1[1][5], arr1[1][6]);
-
-
-let namePerson = prompt("Ввести Артем или Александр");
-
-namePerson == "Артем" ? console.log("директор") :
-    namePerson == "Александр" ? console.log("преподаватель") :
-    console.log("студент");
