a64c545772d3d6ede3d020a02fa6b7b0cba7f06a
Danila Marychev <marychtv5@gmail.com>
2021-10-10 04:46:22 +0300

    два усложненных задания в одном

--- difficult.js
+++ difficult.js
@@ -1,31 +1,5 @@
 'use strict';
 
-// Задание номер 1 усложненное
-
-const isNumber = function (num) {
-    while (num === null) {
-        return 0;
-    }
-    while (!isNaN(parseFloat(num)) && isFinite(num)) {
-        return Number(num);
-    }
-}
-
-let sum = prompt("Напишите сюда числовое значение!");
-
-const asking = function () {
-    while (!isNumber(sum)) {
-        if (isNumber(sum) === 0) {
-            break;
-        }
-
-        sum = prompt("Напишите сюда числовое значение!");
-    }
-}
-asking();
-
-let number = isNumber(sum);
-console.log(number);
 
 
 // Задание номер 2 усложненное часть первая
