3b38b3135457ec86f5ab9c401ddc4604ba5c6e4f
Danila Marychev <marychtv5@gmail.com>
2021-10-24 01:42:08 +0300

    diff commit

--- difficult.js
+++ difficult.js
@@ -1,29 +1,98 @@
 'use strict';
+// Вывести дату в двух форматах 
+// Первый формат  задание (a)
+let date = new Date(2020, 1, 4, 21, 5, 33);
+const months = [
+    'января',
+    'февраля',
+    'марта',
+    'апреля',
+    'мая',
+    'июня',
+    'июля',
+    'августа',
+    'сентября',
+    'октября',
+    'ноября',
+    'декабря'
+];
 
-let week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
-let date = new Date();
-let weekday = date.getDay();
-let options = {
-    weekday: 'long'
-};
-let ourday = new Intl.DateTimeFormat('ru-RU', options).format(date);
+const days = [
+    'Воскресенье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+]
 
+const monthIndex = date.getMonth();
+const monthName = months[monthIndex];
+const yearName = date.getFullYear();
+const dayIndex = date.getDay();
+const dayName = days[dayIndex];
+const hourName = date.getHours();
+const minuteName = date.getMinutes();
+const secondsName = date.getSeconds();
+const dateName = date.getDate();
+let declination;
 
+function addDeclination(a) {
 
-const result = week.find(function (item, index, week) {
-    if (ourday == item) {
-        return index;
+    if (a == 1 || a == 21) {
+        return declination = "час";
+    } else if (a > 1 && a < 5 || a > 21) {
+        return declination = "часа";
+    } else if (a > 4 && a < 21) {
+        return declination = "часов";
     }
-})
-
-for (let i = 0; i < week.length; i++) {
-
-    if (result == week[i]) {
-        document.writeln(week[i].bold() + "<br>");
-        continue;
-    } else if (week[i] == "суббота" || week[i] == "воскресенье" && week[i] != result) {
-        document.writeln(week[i].italics() + "<br>");
-        continue;
-    }
-    document.writeln(week[i] + "<br>");
 }
+
+document.write('Сегодня' + " " + dayName + ", " + dateName + " " + monthName + " " + yearName + " года, " + hourName + " " + addDeclination(date.getHours()) + " " + minuteName + " минут " + secondsName + " секунды ");
+
+
+
+
+console.log(monthIndex);
+console.log(monthName);
+console.log(yearName);
+console.log(dayIndex);
+console.log(dayName);
+console.log(minuteName);
+console.log(secondsName);
+console.log(dateName);
+
+
+// Задание (б)
+function addLeadingZero(d) {
+    return (d < 10) ? '0' + d : d;
+}
+
+let year = date.getFullYear();
+let month2 = addLeadingZero(date.getMonth() + 1);
+let dateofDate = addLeadingZero(date.getDate());
+let hour = date.getHours();
+let minutes = addLeadingZero(date.getMinutes());
+let sec = date.getSeconds();
+
+
+
+document.write("   |   " + dateofDate + "." + month2 + "." + year + " - " + hour + ":" + minutes + ":" + sec);
+
+console.log(year);
+console.log(month2);
+console.log(dateofDate);
+console.log(hour);
+console.log(minutes);
+console.log(sec);
+
+// Обновление дат и времени 
+function displayTime() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    document.write("   " + time);
+}
+
+displayTime();
+const createClock = setInterval(displayTime, 1000);
